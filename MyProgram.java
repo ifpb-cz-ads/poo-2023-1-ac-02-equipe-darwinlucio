b2183036a4a69e62355ed5b89ca45e1decd708d1
João Darwin <107586789+Joao-Darwin@users.noreply.github.com>
2023-03-22 10:10:14 -0300

    Create MyProgram.java

---
+++ MyProgram.java
@@ -0,0 +1,6 @@
+public class MyProgram {
+        public static void main(String[] args){
+                //Questão 03 Atividade 01
+                System.out.println("Terminei a primeira aula com um programa Java!");
+        }
+}
