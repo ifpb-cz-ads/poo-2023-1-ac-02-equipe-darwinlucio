69051bdd0da55fc98cfb06a98d9a854e39d21e00
João Darwin <107586789+Joao-Darwin@users.noreply.github.com>
2023-03-22 10:29:01 -0300

    Create MyProgram2.java

---
+++ MyProgram2.java
@@ -0,0 +1,6 @@
+public class MyProgram2 {
+        public static void main(String[] args){
+                System.out.println("Meu nome: Joao Darwin");
+                System.out.println("Meu time: Real Madrid");
+        }
+}
